fd87d4de6591192a5f391c277e5493e46a3fc43f
apkadmin <nguyenvanan11061997@gmail.com>
2021-02-20 10:41:12 +0700

    update file 1

--- uploaded.php
+++ uploaded.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Home</title>
 </head>
 <body>
+	<div>hello</div>
 		<?php 
 		 $mydir = 'uploads/'; 
-		  
-		  $myfiles = array_diff(scandir($mydir), array('.', '..')); 
-		  
+		 $myfiles = array_diff(scandir($mydir), array('.', '..')); 
 		foreach ($myfiles as $key => $value) {
 			echo '<img src="/uploads/'.$value.'"  style="width:100%;">';
 		 };
-
  ?>
 
 </body>
